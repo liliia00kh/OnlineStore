bc0710d817b7032ac6ab89c4788734804bd8af12
Liliia Khimiak <liliia00khimiak@gmail.com>
2025-09-30 06:27:42 +0300

    make code generic

--- OnlineStore/DependencyInjection.cs
+++ OnlineStore/DependencyInjection.cs
@@ -24,27 +24,8 @@ namespace OnlineStore
                     });
             });
 
-            // EF Core
-            // Select the connection string based on the environment
-            string defaultConnection;
-
-            if (builder.Environment.IsDevelopment() && Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") != "true")
-            {
-                // Local run (Visual Studio / dotnet run)
-                defaultConnection = builder.Configuration.GetConnectionString("DefaultConnection");
-            }
-            else
-            {
-                // Running in Docker or in Production
-                // Prefer the connection string from the environment variable if available,
-                // otherwise fall back to the one in appsettings.json
-                defaultConnection = Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection")
-                                    ?? builder.Configuration.GetConnectionString("DefaultConnection");
-            }
-
-            // Configure Entity Framework to use SQL Server with the selected connection string
             builder.Services.AddDbContext<OnlineStoreDbContext>(options =>
-                options.UseSqlServer(defaultConnection));
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             builder.Services.AddScoped<IUnitOfWork<OnlineStoreDbContext>, UnitOfWork>();
             builder.Services.AddScoped<IProductService, ProductService>();
